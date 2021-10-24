fe1d711655847001ecc053cfc8b60b46e03b3d69
lu <1606537102@qq.com>
2021-10-25 02:28:18 +0800

    A spring boot web service with MySQL

---
+++ src/main/java/com/example/webmybatis/com/example/webmybatis/WebmybatisApplication.java
@@ -0,0 +1,13 @@
+package com.example.webmybatis.com.example.webmybatis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebmybatisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebmybatisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/webmybatis/WebmybatisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webmybatis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebmybatisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
